ecf8ff50895666d4e0a3413936a2ec82900e0f59
Lowis <157913738+Lowis-ar@users.noreply.github.com>
2026-04-18 19:47:49 -0600

    pequeño cambio en el main

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: MainFoodPage() ,
     );
