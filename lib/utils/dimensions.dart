ac9f626faeff223a0cf324364d8b0e7f95aa8055
Lowis <157913738+Lowis-ar@users.noreply.github.com>
2026-05-03 13:04:09 -0600

    solución de problema al iniciar app

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,52 +1,49 @@
 import 'package:get/get.dart';
 
 class Dimensions {
-  static double screenHeight = Get.context!.height;
-  static double screenWidth = Get.context!.width;
+  static double get screenHeight => Get.context != null && Get.context!.height > 0 ? Get.context!.height : 844.0;
+  static double get screenWidth => Get.context != null && Get.context!.width > 0 ? Get.context!.width : 390.0;
 
-  static double pageViewContainer = screenHeight / 3.84;
-  static double pageViewTextContainer = screenHeight / 7.03;
-  static double pageView = screenHeight / 2.64;
+  static double get pageViewContainer => screenHeight / 3.84;
+  static double get pageViewTextContainer => screenHeight / 7.03;
+  static double get pageView => screenHeight / 2.64;
 
   //height dinamico para padding y margen
-  static double height10 = screenHeight / 84.4;
-  static double height15 = screenHeight / 56.27;
-  static double height20 = screenHeight / 42.2;
-  static double height30 = screenHeight / 28.13;
-  static double height45 = screenHeight / 18.76;
-
+  static double get height10 => screenHeight / 84.4;
+  static double get height15 => screenHeight / 56.27;
+  static double get height20 => screenHeight / 42.2;
+  static double get height30 => screenHeight / 28.13;
+  static double get height45 => screenHeight / 18.76;
 
   //width dinamico para padding y margen
-  static double width10 = screenHeight / 84.4;
-  static double width15 = screenHeight / 56.27;
-  static double width20 = screenHeight / 42.2;
-  static double width30 = screenHeight / 28.13;
+  static double get width10 => screenHeight / 84.4;
+  static double get width15 => screenHeight / 56.27;
+  static double get width20 => screenHeight / 42.2;
+  static double get width30 => screenHeight / 28.13;
 
   //tamaño letra
-  static double font20 = screenHeight / 42.2;
-  static double font26 = screenHeight / 32.46;
-  static double font16 = screenHeight / 52.75;
-  static double font18 = screenHeight / 41.28;
+  static double get font20 => screenHeight / 42.2;
+  static double get font26 => screenHeight / 32.46;
+  static double get font16 => screenHeight / 52.75;
+  static double get font18 => screenHeight / 41.28;
 
-  static double radius20 = screenHeight / 42.2;
-  static double radius30 = screenHeight / 28.13;
-  static double radius15 = screenHeight / 56.27;
+  static double get radius20 => screenHeight / 42.2;
+  static double get radius30 => screenHeight / 28.13;
+  static double get radius15 => screenHeight / 56.27;
 
   //size de iconos
-  static double iconSize24 = screenHeight / 35.17;
-  static double iconSize16 = screenHeight / 52.75;
-  static double iconSize26 = screenHeight / 39.04;
+  static double get iconSize24 => screenHeight / 35.17;
+  static double get iconSize16 => screenHeight / 52.75;
+  static double get iconSize26 => screenHeight / 39.04;
 
   //listView
-  static double listViewImgSize = screenWidth / 3.25;
-  static double listViewTextContSize = screenWidth / 3.9;
+  static double get listViewImgSize => screenWidth / 3.25;
+  static double get listViewTextContSize => screenWidth / 3.9;
 
   //popular food
-  static double popularFoodImgSize = screenHeight / 2.41;
+  static double get popularFoodImgSize => screenHeight / 2.41;
 
   //botones
-  static double bottomHeightBar = screenHeight / 7.03;
-
+  static double get bottomHeightBar => screenHeight / 7.03;
 }
-
 
